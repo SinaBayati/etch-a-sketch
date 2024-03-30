d29ba005a706ffee4843767e057fef695844f4d7
Sina Bayati <bayati.sina.w@gmail.com>
2024-03-30 04:54:31 +0330

    Fix renderTable function
    The renderTable function now creates correct number
    of cells out of the given dimension.

--- js/script.js
+++ js/script.js
@@ -5,9 +5,14 @@ function renderTable(dimension){
   container.className = "container";
   
   for(let i = 0; i < dimension; i++){
-    let divElem = document.createElement("div");
-    divElem.className = "cell";
-    container.appendChild(divElem);  
+    let rowElem = document.createElement("div");
+    rowElem.className = "row";
+    for(let j = 0; j < dimension; j++){
+      let divElem = document.createElement("div");
+      divElem.className = "cell";
+      rowElem.appendChild(divElem);
+    }
+    container.appendChild(rowElem);  
   }
 
   document.body.appendChild(container);
